7934a4dc9798da0b58732959b5bc1447dfe1939a
Stratadox <info@stratadox.com>
2018-06-17 17:04:01 +0200

    Add the hydration data to the observation payload

--- src/ObservesHydration.php
+++ src/ObservesHydration.php
@@ -14,6 +14,7 @@ interface ObservesHydration
      * This method will be called by the hydrator on the registered observer.
      *
      * @param object $theInstance The instance that is being hydrated.
+     * @param array  $theData     The data that is written to the instance.
      */
-    public function hydrating(object $theInstance) : void;
+    public function hydrating(object $theInstance, array $theData) : void;
 }
